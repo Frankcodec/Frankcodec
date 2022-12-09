28414bbede250028f9a265939f0c0e23b15b56dc
Obi Franklin <118486147+Frankcodec@users.noreply.github.com>
2022-12-10 00:58:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+  <!DOCTYPE html>
+<html>
+<head>
+	<title>Where the marketplace comes to you-Surge</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="" href="./bootstrap/fontawesome-free-6.2.0-web/css/all.css">
+	<script src="./bootstrap/fontawesome-free-6.2.0-web/js/all.js"></script>
+	<script src="./bootstrap/fontawesome-free-6.2.0-web/css/all.css"></script>
+</head>
+<body id="body">
+	<nav style="width: 70%;">
+		<h2>Surge</h2>
+		<div>
+			<a href=""><h3 class="sef">Store</h3></a>
+			<a href=""><h3 class="sef">Start-Selling</h3></a>
+			<a href=""><h3 class="sef">Register</h3></a>
+		</div>
+	</nav>
+	<section id="section1">
+		<div class="left">
+			<h1>Open your <span style="color: #047aed;">online store</span> and start selling your <span style="color: #047aed">products</span> in minutes</h1>
+			<a href="signup.php"><button style="font-size: 16px; font-family: sans-serif; color: #fff; background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); margin: 0;  width: 165px; height: 65px; border-radius: 15px; border: none;">Get Started ></button></a>
+		</div>
+		<div class="phone">
+			<div class="iphone"></div>
+			<div class="item" style="margin-bottom: 5%;">
+				<img src="./images/blender.png" style="width: 89px;height: 175px;margin-right: 4px;">
+				<img src="./images/shirt.png" style="width: 89px;height: 175px;margin-right: 4px;">
+				<img src="./images/shoe.png" style="width: 89px;height: 175px;">
+			</div>
+			<div class="item2" style="margin-bottom: 5%;">
+				<img src="./images/blender.png" style="width: 89px;height: 175px;margin-right: 4px;">
+				<img src="./images/shirt.png" style="width: 89px;height: 175px;margin-right: 4px;">
+				<img src="./images/shoe.png" style="width: 89px;height: 175px;">
+			</div>
+			<hr>
+			<button style="align-self: center; width: 125px; height: 45px; background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); color: #fff; border-radius: 15px; margin: 34px 28%;border: none;"> Shop Now</button>
+			<div class="line"></div>
+		</div>
+	</section>
+	<div style="width: 45px; height: 45px; border-radius: 50%; background-color: grey; position: fixed; padding: 5px; margin-left: 90%; display: flex; flex-direction: row; justify-content: center; align-items: center;">
+		<i class="fa fa-message fa-2x" id="icon"></i>
+	</div>
+	<div id="subsec">
+	<section id="section2" style="background: linear-gradient(45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);animation: sell 10s linear infinite">
+		<div class="left2" style="animation: shows 12s linear infinite;">
+			<div class="one" style="color: #212;">
+				<p>James Uche</p>
+				<h3>I had the best experience using Surge, they offer top notch services and that never stops to impress me</h3>
+			</div>
+			<div class="two" style="color: #212;">
+				<p>Cynthia Oma</p>
+				<h3>Signing up for Surge was the best thing that probably ever happened to me</h3>
+			</div>
+			<div class="three"style="color: #212;">
+				<p>Adebayo Olamide</p>
+				<h3>surge made me realize how easy and stress free selling is</h3>
+			</div>
+			<div class="four" style="color: #212;">
+				<p>Ahmed Quadri</p>
+				<h3>I had the best experience using Surge, they offer top notch services and that never stops to impress me</h3>
+			</div>
+		</div>
+			<div class="right2">
+			<h1>Check out the reviews from our customers</h1>
+			<a href="#"><button style="align-self: center; width: 125px; height: 45px; background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); color: #fff; border-radius: 15px; border: none;">Check now</button></a>
+		</div>
+	</section>
+	<section class="seperated">
+		<h1 style="font-family: Millik; color: #000; font-size: 45px;">Why choose <span style="font-family: Millik; color: #047aed;">Surge</span></h1>
+	 <div class="seperate">
+   		 <div class="divs"><div style="animation: moves 10s linear infinite;background: linear-gradient(-45deg, #F5EDED, #F5EDED, #3D3C3C, #312F2F);background-size: 400% 400%;display: flex;flex-direction: column;align-items: center;justify-content: center;width: 100%;height: 195px;">
+   		 	<?php include_once 'phones.svg'; ?>
+   		 </div>
+			
+			<h3 style="text-align: left;font-family: sans-serif;margin: 15px 35px;font-style: italic;">With <span style="color: #047aed;">Surge,</span> You need not worry about lifting a finger as the products can be deliverd right to your doorstep</h3>
+		 </div>
+		 <div class="divs" style="background-color: whitesmoke;">
+		 	<div style="display: flex;flex-direction: column;align-items: center;justify-content: center;width: 100%;height: 165px;background-color: whitesmoke;transform: translateY(21%);">
+		 		<?php include_once 'secured.svg'; ?>
+		 	</div>
+		 	<div style="background-color: white;height: 355px;margin-top: 0;">
+			<h2 style="margin-top: 0;">We </h2>
+		</div>
+		 </div>
+		 <div class="divs">
+		 	<div style="display: flex;flex-direction: column;align-items: center;justify-content: center;width: 100%;height: 195px;background-color: whitesmoke;">
+		 		<?php include_once 'free.svg' ?>
+		 	</div>
+			<h2>We </h2>
+		 </div>
+     </div>
+    </section>
+   <section style="overflow: hidden;background-color: #1c1b1b" class="edit">
+   	<h1 style="color: #fff;font-size: 38px;text-align: center;font-family: millik;margin-bottom: 5%;">Upgrade to <span style="color: #047aed">Surge</span> Premium</h1>
+   		<div style="display: flex;flex-direction: row;">
+   			
+   			<div style="margin-left: 5%;">
+   				<ul class="lones1">
+   					<li style="animation-delay: 0.3s;"></li>
+   				</ul>
+   				<ul class="lones2">
+   					<li style="animation-delay: 0.5s;"></li>
+   					<li style="animation-delay: 0.6s;"></li>
+   					<li style="animation-delay: 0.7s;"></li>
+   					<li style="animation-delay: 0.8s;"></li>
+   					<li style="animation-delay: 0.9s;"></li>
+   				</ul>
+   				<ul class="lones1" >
+   					<li  style="animation-delay: 1s;"></li>
+   				</ul>
+   				<ul class="lones2" >
+   					<li style="animation-delay: 1.1s;"></li>
+   					<li style="animation-delay: 1.2s;"></li>
+   					<li style="animation-delay: 1.3s;"></li>
+   					<li style="animation-delay: 1.4s;"></li>
+   					<li style="animation-delay: 1.5s;"></li>
+   				</ul>
+   				<ul class="lones1">
+   					<li  style="animation-delay: 1.6s;"></li>
+   				</ul>
+   				<ul class="lones2">
+   					<li style="animation-delay: 1.7s;"></li>
+   					<li style="animation-delay: 1.8s;"></li>
+   					<li style="animation-delay: 1.9s;"></li>
+   					<li style="animation-delay: 2s;"></li>
+   					<li style="animation-delay: 2.1s;"></li>
+   				</ul>
+   			</div>
+   			<div class="golds" style="margin-left: 12%;">
+   				<h1 style="color: #fff;margin-bottom: 12%;animation-delay: 0.3s;color: transparent;">Have access to unlimited customers</h1>
+   				<h5 style="color: #fff;animation-delay: 0.5s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 0.6s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 0.7s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 0.8s;color: transparent;">Be among first search results</h5>
+   				<h1 style="color: #fff;margin-bottom: 9%;margin-top: 13%;animation-delay: 1s;color: transparent;">Have access to unlimited customers</h1>
+   				<h5 style="color: #fff;animation-delay: 1.1s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 1.2s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 1.3s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 1.4s;color: transparent;">Be among first search results</h5>
+   				<h1 style="color: transparent;margin-bottom: 7%;margin-top: 11%;animation-delay: 1.6s;color: transparent;">Have access to unlimited customers</h1>
+   				<h5 style="color: #fff;animation-delay: 1.7s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 1.8s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 1.9s;color: transparent;">Be among first search results</h5>
+   				<h5 style="color: #fff;animation-delay: 2s;color: transparent;">Be among first search results</h5>
+   			</div>
+   			<div style="border-radius: 50%;overflow: hidden;width: 495px;height: 495px;background-color: #612b85;margin-left: 19%;" class="prettys">
+   			<marquee><div class="nicejob" style="position: absolute;margin-top: 45%;margin-left: -55%;"></div><div class="nicejob" style="position: absolute;margin-top: 35%;margin-left: 65%;"></div><div class="nicejob" style="position: absolute;margin-top: 55%;margin-left: 75%;"></div><div class="nicejob" style="position: absolute;margin-top: 25%;margin-left: 35%;"></div><div class="nicejob" style="position: absolute;margin-top: 25%;margin-left: 85%;"></div><div class="nicejob" style="position: absolute;margin-top: 55%;margin-left: 105%;"></div><div class="nicejob" style="position: absolute;margin-top: 75%;margin-left: 135%;"></div><div class="nicejob" style="position: absolute;margin-top: 25%;margin-left: 155%;"></div><div class="nicejob" style="position: absolute;margin-top: 45%;margin-left: 55%;"></div><div class="nicejob" style="position: absolute;margin-top: 35%;margin-left: 185%;"></div><div class="nicejob" style="position: absolute;margin-top: 55%;margin-left: 205%;"></div><div class="nicejob" style="position: absolute;margin-top: 25%;margin-left: 235%;"></div><img src="images/globe.png" style="background-color: #612b85;width: 1525px;height: 565px;margin-left: -55px;position: relative;z-index: 2;"></marquee>
+   		</div>
+   		
+   		</div>
+   </section>
+   <footer>
+   	<div style="display: flex;flex-direction: column;width: 70%;margin-left: 15%;margin-right: 15%;">
+   		<div style="display: flex;flex-direction: row;width: 100%;justify-content: space-around;align-items: center;position: relative;" class="heat">
+   			<div style="margin: 25px;">
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   		</div>
+   		<div style="margin: 25px;">
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   		</div>
+   		<div style="margin: 25px;">
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   		</div>
+   		<div style="margin: 25px;">
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   			<a href=""><h3>About</h3></a>
+   		</div>
+   	</div>
+   	<div style="align-self: flex-end;display: flex;flex-direction: row;" class="dope">
+   	    <div style="width: 21px; height: 21px; border-radius: 50%; background-color: white;  padding: 5px;border: 2px solid black;">
+		<i class="fab fa-facebook-f" id="icons"></i></div>
+		<div style="width: 21px; height: 21px; border-radius: 50%; background-color: white;  padding: 5px;border: 2px solid black;">
+		<i class="fab fa-twitter" id="icons"></i>
+	</div>
+	<div style="width: 21px; height: 21px; border-radius: 50%; background-color: white;  padding: 5px;border: 2px solid black;">
+		<i class="fab fa-whatsapp" id="icons"></i>
+	</div>
+	<div style="width: 21px; height: 21px; border-radius: 50%; background-color: white;  padding: 5px;border: 2px solid black;margin-right: 87px;">
+		<i class="fab fa-instagram" id="icons"></i>
+	</div>
+	</div>
+</div>
+   </footer>
+</body> 
+<style type="text/css">
+	
+</style>
+<script type="text/javascript">
+(function () {
+	var elements;
+	var tableu;
+	var linesa;
+	var linesb;
+	var windowHeight;
+	function init() {
+		elements = document.querySelectorAll('.divs');
+		tableu = document.querySelectorAll('.golds');
+		linesa = document.querySelectorAll('.lones1');
+		linesb = document.querySelectorAll('.lones2');
+		prettys = document.querySelectorAll('.prettys');
+		windowHeight = window.innerHeight;
+	}
+	function checkPosition() {
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var positionFromTop = elements[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight <= 0) {
+				element.classList.add('divone');
+				element.classList.remove('divs');
+			}
+			else{
+				element.classList.add('divs');
+				element.classList.remove('divone');
+			}
+		}
+		for (var j = 0; j < tableu.length; j++) {
+			var tableux = tableu[j];
+			var positionFromTop = tableu[j].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight <= 0) {
+				tableux.classList.add('gold');
+				tableux.classList.remove('golds');
+			}
+			else{
+				tableux.classList.add('golds');
+				tableux.classList.remove('gold');
+			}
+		}
+		for (var i = 0; i < linesa.length; i++) {
+			var linea = linesa[i];
+			var positionFromTop = linesa[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight <= 0) {
+				linea.classList.add('lines1');
+				linea.classList.remove('lones1');
+			}
+			else{
+				linea.classList.add('lones1');
+				linea.classList.remove('lines1');
+			}
+		}
+		for (var i = 0; i < linesb.length; i++) {
+			var lineb = linesb[i];
+			var positionFromTop = linesb[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight <= 0) {
+				lineb.classList.add('lines2');
+				lineb.classList.remove('lones2');
+			}
+			else{
+				lineb.classList.add('lones2');
+				lineb.classList.remove('lines2');
+			}
+		}
+		for (var i = 0; i < prettys.length; i++) {
+			var pretty = prettys[i];
+			var positionFromTop = prettys[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight <= 0) {
+				pretty.classList.add('pretty');
+				pretty.classList.remove('prettys');
+			}
+			else{
+				pretty.classList.add('prettys');
+				pretty.classList.remove('pretty');
+			}
+		}
+	}
+	
+	window.addEventListener('scroll', checkPosition);
+	window.addEventListener('resize', init);
+
+	init();
+	checkPosition();
+})();
+
+</script>
+</html>
